ffb1ad316adf0b30ac7512ac8a95b8fe996d36e1
RGM3804A <max.rogel@univ-tlse3.fr>
2024-04-04 17:02:52 +0200

    Test Prendre étal

--- test/controleur/ControlPrendreEtalTest.java
+++ test/controleur/ControlPrendreEtalTest.java
@@ -11,7 +11,7 @@ import villagegaulois.Village;
 class ControlPrendreEtalTest {
 	private Village village;
 	private Chef abraracourcix;
-	private ControlVerifierIdentite controlVerifierIdentite;
+	//private ControlVerifierIdentite controlVerifierIdentite;
 	
 	@BeforeEach
 	public void initialiserSituation(){
@@ -19,31 +19,38 @@ class ControlPrendreEtalTest {
 		village = new Village("le village des irréductibles", 10, 5);
 		abraracourcix = new Chef("Abraracourcix", 10, village);
 		village.setChef(abraracourcix);
-		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		//ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
 	}
 	
 	@Test
 	void testControlPrendreEtal() {
-		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(new ControlVerifierIdentite(village),village);
 		assertNotNull(controlPrendreEtal, "Constructeur ne renvoie pas null");
 	}
 
 	@Test
 	void testResteEtals() {
-		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(new ControlVerifierIdentite(village),village);
 		assertTrue(controlPrendreEtal.resteEtals());
-		
+		controlPrendreEtal.prendreEtal("Asterix", "glaive", 10);
+		controlPrendreEtal.prendreEtal("Obelix", "menhir", 10);
+		controlPrendreEtal.prendreEtal("Abraracourcix", "glaive", 10);
+		controlPrendreEtal.prendreEtal("Bonemine", "fleurs", 10);
+		controlPrendreEtal.prendreEtal("Panoramix", "chaudron", 10);
+		assertFalse(controlPrendreEtal.resteEtals());
 	}
 
 	@Test
 	void testPrendreEtal() {
-		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(new ControlVerifierIdentite(village),village);
 		assertTrue(controlPrendreEtal.prendreEtal("Asterix", "Glaive", 10) == 0);
 	}
 
 	@Test
 	void testVerifierIdentite() {
-		fail("Not yet implemented");
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(new ControlVerifierIdentite(village),village);
+		assertTrue(controlPrendreEtal.verifierIdentite("Abraracourcix"));
+		assertFalse(controlPrendreEtal.verifierIdentite("Asterix"));
 	}
 
 }
