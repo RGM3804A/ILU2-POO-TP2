a74d1f6711e102a75c2a2592f49a83c2b84d92d8
RGM3804A <max.rogel@univ-tlse3.fr>
2024-04-04 16:25:01 +0200

    Réinsertion des tests après perte

---
+++ test/controleur/ControlPrendreEtalTest.java
@@ -0,0 +1,49 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlPrendreEtalTest {
+	private Village village;
+	private Chef abraracourcix;
+	private ControlVerifierIdentite controlVerifierIdentite;
+	
+	@BeforeEach
+	public void initialiserSituation(){
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+	}
+	
+	@Test
+	void testControlPrendreEtal() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		assertNotNull(controlPrendreEtal, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testResteEtals() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		assertTrue(controlPrendreEtal.resteEtals());
+		
+	}
+
+	@Test
+	void testPrendreEtal() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite,village);
+		assertTrue(controlPrendreEtal.prendreEtal("Asterix", "Glaive", 10) == 0);
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		fail("Not yet implemented");
+	}
+
+}
